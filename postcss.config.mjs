a481f8e354e8149a1fb970751a077d15b696bda4
yujunsun0 <yujunsun0@gmail.com>
2025-12-17 17:43:54 +0900

    fix : PostCSS v4 방식으로 업데이트

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,9 @@
 // 📌 TypeScript가 아닌 .mjs / .js에서도 IDE 지원을 받기 위한 타입 선언
 /** @type {import('postcss-load-config').Config} */
 const config = {
-    // CSS를 어떻게 “가공”할지 정의하는 파이프라인
+  // CSS를 어떻게 "가공"할지 정의하는 파이프라인
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
